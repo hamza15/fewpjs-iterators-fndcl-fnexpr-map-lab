d6671981d23fab2628384b9b9c480f1084b53c84
hamza15 <sardan5@vt.edu>
2021-02-23 16:04:30 -0500

    Done.

--- index.js
+++ index.js
@@ -12,5 +12,14 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  return tutorials
+  return tutorials.map(titleCase)
 }
+
+function titleCase(str) {
+  return str.split(' ').map((word) => {
+    return (word.charAt(0).toUpperCase() + word.slice(1));
+  }).join(' ');
+}
+// const titleCased = tutorials.map(function(tutorial) {
+//   return tutorial.charAt(0).toUpperCase()
+// })
